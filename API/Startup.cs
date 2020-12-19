f7b9f730852a107aa14f33acd3f7d1d18b13dde0
Shayan Vaghei <shayan546@gmail.com>
2020-12-19 15:32:26 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,27 +14,26 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace API
-{
-    public class Startup
-    {
+namespace API {
+    public class Startup {
         private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
-        {
+        public Startup (IConfiguration config) {
             _config = config;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
 
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<DataContext> (options => {
+                options.UseSqlite (_config.GetConnectionString ("DefaultConnection"));
             });
-            services.AddControllers();
+            services.AddControllers ();
+
+            // adding Cors
+            services.AddCors ();
+
             //services.AddSwaggerGen(c =>
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -42,24 +41,24 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
                 //app.UseSwagger();
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            // add Cors here in the pipeline
+            app.UseCors (policy => policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("https://localhost:4200"));
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseAuthorization ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'the dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (resposne) => {
+        this.users = resposne;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
