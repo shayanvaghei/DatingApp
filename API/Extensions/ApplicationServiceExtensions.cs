059de7184b90bd9c99b39b824aecdf9e83795a2d
Shayan Vaghei <shayan546@gmail.com>
2021-01-26 22:10:50 -0500

    End of the course

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,8 @@
-﻿using API.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
@@ -7,29 +11,50 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace API.Extensions
-{
-    public static class ApplicationServiceExtensions
-    {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddSingleton<PresenceTracker>();  // added for signalR for tracking OnlineUsers
-            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-            services.AddScoped<ITokenService, TokenService>();
+namespace API.Extensions {
+    public static class ApplicationServiceExtensions {
+        public static IServiceCollection AddApplicationServices (this IServiceCollection services, IConfiguration config) {
+            services.AddSingleton<PresenceTracker> (); // added for signalR for tracking OnlineUsers
+            services.Configure<CloudinarySettings> (config.GetSection ("CloudinarySettings"));
+            services.AddScoped<ITokenService, TokenService> ();
             // add service for our repository
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddScoped<IPhotoService, PhotoService>();
-            services.AddScoped<LogUserActivity>();
+            services.AddScoped<IUnitOfWork, UnitOfWork> ();
+            services.AddScoped<IPhotoService, PhotoService> ();
+            services.AddScoped<LogUserActivity> ();
             // ading automapper services
-            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            services.AddAutoMapper (typeof (AutoMapperProfiles).Assembly);
+            services.AddDbContext<DataContext> (options => {
+                var env = Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                // Depending on if in development or production, use either Heroku-provided
+                // connection string, or development connection string from env var.
+                if (env == "Development") {
+                    // Use connection string from file.
+                    connStr = config.GetConnectionString ("DefaultConnection");
+                } else {
+                    // Use connection string provided at runtime by Heroku.
+                    var connUrl = Environment.GetEnvironmentVariable ("DATABASE_URL");
+
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace ("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split ("@") [0];
+                    var pgHostPortDb = connUrl.Split ("@") [1];
+                    var pgHostPort = pgHostPortDb.Split ("/") [0];
+                    var pgDb = pgHostPortDb.Split ("/") [1];
+                    var pgUser = pgUserPass.Split (":") [0];
+                    var pgPass = pgUserPass.Split (":") [1];
+                    var pgHost = pgHostPort.Split (":") [0];
+                    var pgPort = pgHostPort.Split (":") [1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                }
+
+                // Whether the connection string came from the local development configuration file
+                // or from the environment variable from Heroku, use it to set up your DbContext.
+                options.UseNpgsql (connStr);
             });
 
             return services;
